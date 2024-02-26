f06ad8b60d0b3142103c86ee27ac6cba4a0f92e6
Jhonathan Vinicius <106465098+JhonathanVN@users.noreply.github.com>
2024-02-26 19:35:43 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,76 @@
+function img1(){
+    document.getElementById("imagem").src = "imagens/Homem.jpg"
+    document.getElementById("par").innerHTML = "<p>Salve meu nobre.&#129309;</p>"
+    if(imagem.style.display == 'block' && par.style.display == 'block'){
+        imagem.style.display = 'block'
+        par.style.display = 'block'
+        fecha.style.display = 'block'
+    }else{
+        imagem.style.display = 'block'
+        par.style.display = 'block'
+        fecha.style.display = 'block'
+    }
+}
+function img2(){
+    document.getElementById("imagem").src = "imagens/Mulher.jpg"
+    document.getElementById("par").innerHTML = "<p>Olá bela dama.&#128081;</p>"
+    if(imagem.style.display == 'block' && par.style.display == 'block'){
+        imagem.style.display = 'block'
+        par.style.display = 'block'
+        fecha.style.display = 'block'
+    }else{
+        imagem.style.display = 'block'
+        par.style.display = 'block'
+        fecha.style.display = 'block'
+    }
+}
+function img3(){
+    document.getElementById("imagem").src = "imagens/LGBT.jpg"
+    document.getElementById("par").innerHTML = "<p>Eae&#128077;</p>"
+    if(imagem.style.display == 'block' && par.style.display == 'block'){
+        imagem.style.display = 'block'
+        par.style.display = 'block'
+        fecha.style.display = 'block'
+    }else{
+        imagem.style.display = 'block'
+        par.style.display = 'block'
+        fecha.style.display = 'block'
+    }
+}
+function img4(){
+    document.getElementById("imagem").src = "imagens/Civic.jpg"
+    document.getElementById("par").innerHTML = "<p>VRUUUUUUUUUUMMMMMMMM&#128663;</p>"
+    if(imagem.style.display == 'block' && par.style.display == 'block'){
+        imagem.style.display = 'block'
+        par.style.display = 'block'
+        fecha.style.display = 'block'
+    }else{
+        imagem.style.display = 'block'
+        par.style.display = 'block'
+        fecha.style.display = 'block'
+    }
+}
+function img5(){
+    document.getElementById("imagem").src = "imagens/Helicopt.jpg"
+    document.getElementById("par").innerHTML = "<p>VUP VUP VUP VUP VUP VUP VUP &#128641;</p>" + "<p>POW POW POW POW POW POW POW&#128165;</p>" 
+    if(imagem.style.display == 'block' && par.style.display == 'block'){
+        imagem.style.display = 'block'
+        par.style.display = 'block'
+        fecha.style.display = 'block'
+    }else{
+        imagem.style.display = 'block'
+        par.style.display = 'block'
+        fecha.style.display = 'block'
+    }
+}
+function fechar(){
+    if(imagem.style.display == 'block' && par.style.display == 'block'){
+        imagem.style.display = 'none'
+        par.style.display = 'none'
+        fecha.style.display = 'none'
+    }else{
+        imagem.style.display = 'block'
+        par.style.display = 'block'
+        fecha.style.display = 'none'
+    }
+}
